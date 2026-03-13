3e066e5cdac8ef69e81c83b73d0274b7e664ca5f
Kym Eddiebong Bogani <k.bogani.145167.tc@umindanao.edu.ph>
2026-03-13 13:45:06 +0800

    feat: add Home and Services page links to About Us sidebar with divider separator

--- public_website/app/components/aboutus_components/AboutUsSidebarComponent.php
+++ public_website/app/components/aboutus_components/AboutUsSidebarComponent.php
@@ -8,7 +8,7 @@ require_once __DIR__ . '/../reusable_components/Component.php';
 class AboutUsSidebarComponent extends \Component {
 
     public function render(): string {
-        $navItems = [
+        $sectionItems = [
             ['id' => 'history',   'icon' => 'fa-book-open',      'label' => 'Our Story'],
             ['id' => 'workplace', 'icon' => 'fa-store',           'label' => 'Inside the Shop'],
             ['id' => 'products',  'icon' => 'fa-tshirt',          'label' => 'Our Products'],
@@ -17,14 +17,33 @@ class AboutUsSidebarComponent extends \Component {
             ['id' => 'employees', 'icon' => 'fa-users',           'label' => 'Our Team'],
         ];
 
+        $pageLinks = [
+            ['url' => '?page=home',     'icon' => 'fa-home',  'label' => 'HOME'],
+            ['url' => '?page=services', 'icon' => 'fa-print', 'label' => 'SERVICES'],
+        ];
+
         ob_start();
         ?>
         <aside class="about-sidebar" id="aboutSidebar">
+
+            <!-- Page navigation -->
             <div class="about-sidebar-brand">
                 <span class="about-sidebar-title">ABOUT US</span>
             </div>
+            <nav class="about-sidebar-pages">
+                <?php foreach ($pageLinks as $link): ?>
+                    <a href="<?php echo $link['url']; ?>" class="about-sidebar-page-link">
+                        <i class="fas <?php echo $link['icon']; ?> about-sidebar-icon"></i>
+                        <span><?php echo $link['label']; ?></span>
+                    </a>
+                <?php endforeach; ?>
+            </nav>
+
+            <div class="about-sidebar-divider"></div>
+
+            <!-- Section navigation -->
             <nav class="about-sidebar-nav">
-                <?php foreach ($navItems as $item): ?>
+                <?php foreach ($sectionItems as $item): ?>
                     <a href="#<?php echo $item['id']; ?>"
                        class="about-sidebar-link"
                        data-section="<?php echo $item['id']; ?>">
@@ -33,6 +52,7 @@ class AboutUsSidebarComponent extends \Component {
                     </a>
                 <?php endforeach; ?>
             </nav>
+
         </aside>
         <?php
         return ob_get_clean();
