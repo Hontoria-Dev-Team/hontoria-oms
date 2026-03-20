731ecbc631db2038bcb830c12a45584b3eab3cf1
VictorBSRV <124992559+VictorBSRV@users.noreply.github.com>
2026-03-20 22:17:48 +0800

    mod: Changed Auto Refresher time to 60 secs

--- InternalWeb/.JS/AutoRefresher.js
+++ InternalWeb/.JS/AutoRefresher.js
@@ -4,7 +4,7 @@ function resetIdleTimer() {
     clearTimeout(idleTimer);
     idleTimer = setTimeout(function () {
         location.reload();
-    }, 30000); // 30 seconds of idle
+    }, 60000); // 60 seconds of idle
 }
 
 // Listen for any user activity
