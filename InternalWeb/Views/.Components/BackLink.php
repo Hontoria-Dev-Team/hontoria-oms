bf83dd58d400532f2870cbb16bcd01ee47524f1e
VictorBSRV <124992559+VictorBSRV@users.noreply.github.com>
2026-03-20 22:14:03 +0800

    mod: Changed the text in backlinks to just an arrow

--- InternalWeb/Views/.Components/BackLink.php
+++ InternalWeb/Views/.Components/BackLink.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
-<a href="<?= $backLink; ?>" class="centerColumnLayout importantInput roundedMid tinHeight fitWidth regPadding capitalFirst minFont">
-    Return to <?= $lastPage ?> Page
+<a href="<?= $backLink; ?>" class="centerHoriRowLayout minGap importantInput roundedMid tinHeight fitWidth regPadding capitalFirst minFont">
+    <img src="../../Shared/Img/ArrowIcon.png" alt="Arrow" class="invertColors mirrorX">
+    <h3><?= $lastPage ?> Page</h3>
 </a>
