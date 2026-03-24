4f786b5a6934383a3dbe31f4c3b6859224358f4b
VictorBSRV <124992559+VictorBSRV@users.noreply.github.com>
2026-03-25 00:50:41 +0800

    feat: General JS helpers
    A helper to:
    *Make camelCases strings into normal case text
    *Get the difference between arrays
    *Get the directional difference between arrays

---
+++ InternalWeb/.JS/MiscHelpers.js
@@ -0,0 +1,20 @@
+function formatCamelCase(str) {
+    return str
+        .replace(/([a-z])([A-Z])/g, '$1 $2')
+        .replace(/^./, s => s.toUpperCase());
+}
+
+function getArrayDiff(arr1, arr2, key) {
+    const set1 = new Set(arr1.map(item => item[key]));
+    const set2 = new Set(arr2.map(item => item[key]));
+
+    return [
+        ...arr1.filter(item => !set2.has(item[key])),
+        ...arr2.filter(item => !set1.has(item[key]))
+    ];
+}
+
+function getDirectionalArrayDiff(arr1, arr2, key) {
+    const set2 = new Set(arr2.map(item => item[key]));
+    return arr1.filter(item => !set2.has(item[key]));
+}
