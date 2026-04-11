cf40e6f0691dd6ba4876682dd1ae17f9ab2ff2c7
VictorBSRV <124992559+VictorBSRV@users.noreply.github.com>
2026-04-11 20:31:18 +0800

    mod: Made Error Box at front

--- InternalWeb/Views/.Components/ErrorBox.php
+++ InternalWeb/Views/.Components/ErrorBox.php
@@ -1,5 +1,5 @@
 <?php if (isset($_SESSION['error']) && $_SESSION['error']): ?>
-    <div class="errorMessage centerColumnLayout regPadding">
+    <div class="errorMessage centerColumnLayout regPadding topZ">
         <h3>Error: <?php echo htmlspecialchars($_SESSION['error']); ?></h3>
     </div>
     <?php unset($_SESSION['error']); ?>
