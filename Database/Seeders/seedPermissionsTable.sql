eeaa135eda610f160b1d630cac832bc7866722ad
VictorBSRV <124992559+VictorBSRV@users.noreply.github.com>
2026-04-13 18:33:43 +0800

    mod: Removed SQL regarding old permissions

--- Database/Seeders/seedPermissionsTable.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO permissions (name, description) VALUES
-('canManageStaff', 'Can add and delete staff members, and also grant or revoke permissions.');
-
-INSERT INTO userPermissions (userID, permissionID) VALUES
-(1, 1);
