444ab437b137e3aa612d2ffe3ca61e67f769774b
Kym Eddiebong A. Bogani <k.bogani.145167.tc@umindanao.edu.ph>
2026-04-15 01:36:07 +0800

    refactor(components): convert header and footer PHP classes to static HTML

---
+++ PublicWeb/Views/.Components/SharedComponents/HeaderComponents.php
@@ -0,0 +1,46 @@
+<header class="header" id="header">
+    <div class="header-inner">
+        
+        <div class="logo">
+            <img src="/.Images/logo.jpg" alt="Hontoria Logo" class="logo-img" onerror="this.style.display='none'"/>
+            <div class="logo-text">
+                <span class="logo-name">HONTORIA</span>
+                <span class="logo-sub">PRINTING SERVICES</span>
+            </div>
+        </div>
+        
+        <nav class="nav-desktop">
+            <a href="?page=home" class="nav-link">Home</a>
+            <span class="nav-sep">|</span>
+            <a href="?page=services" class="nav-link">Services</a>
+            <span class="nav-sep">|</span>
+            <a href="?page=about" class="nav-link">About Us</a>
+        </nav>
+        
+        <div class="header-right">
+            <button class="hamburger" id="hamburger" aria-label="Open menu">
+                <span></span><span></span><span></span>
+            </button>
+        </div>
+
+    </div>
+    
+    <div class="order-bar">
+        <span class="order-bar-text">
+            <i class="fab fa-facebook-f"></i>
+            FOR ORDERS CLICK HERE:
+            <a href="https://www.facebook.com/jhong.hontoria.3" target="_blank" class="fb-link">
+                <i class="fab fa-facebook"></i> FACEBOOK PAGE
+            </a>
+        </span>
+    </div>
+    
+    <div class="mobile-nav" id="mobileNav">
+        <button class="close-nav" id="closeNav"><i class="fas fa-times"></i></button>
+        <a href="?page=home" class="mob-link">Home</a>
+        <a href="?page=services" class="mob-link">Services</a>
+        <a href="?page=about" class="mob-link">About Us</a>
+    </div>
+    
+    <div class="overlay" id="overlay"></div>
+</header>
