ca1682e2ab9953ee16f8797431e11ec7763f20f5
Kym Eddiebong A. Bogani <k.bogani.145167.tc@umindanao.edu.ph>
2026-04-14 23:58:24 +0800

    refactor(views): convert Services page to pure HTML template and update asset/page URLs

--- PublicWeb/Controllers/PublicC.php
+++ PublicWeb/Controllers/PublicC.php
@@ -4,21 +4,21 @@ class PublicC {
 
     public function __construct($pdo) {
         require_once __DIR__ . '/../Models/PublicM.php';
-        $this->publicModel = new PublicM($pdo);
+        $this->publicModel = new \PublicM($pdo);
     }
 
     public function showHomePage() {
         $page = "home";
-        require __DIR__ . '/../Views/Home/Page.php';
+        require __DIR__ . '/../Views/Home/HomePage.php';
     }
 
     public function showServicesPage() {
         $page = "services";
-        require __DIR__ . '/../Views/Services/Page.php';
+        require __DIR__ . '/../Views/Services/ServicesPage.php';
     }
 
     public function showAboutUsPage() {
         $page = "about";
-        require __DIR__ . '/../Views/AboutUs/Page.php';
+        require __DIR__ . '/../Views/AboutUs/AboutusPage.php';
     }
 }
