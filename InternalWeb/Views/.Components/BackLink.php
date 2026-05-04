a61ea6290fccc76c0ce7c262b2187b1c049211cd
VictorBSRV <124992559+VictorBSRV@users.noreply.github.com>
2026-05-04 22:09:36 +0800

    add: Added shadows to backlinks

--- InternalWeb/Views/.Components/BackLink.php
+++ InternalWeb/Views/.Components/BackLink.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<a href="<?= $backLink; ?>" class="centerHoriRowLayout minGap importantInput roundedMid tinHeight fitWidth regPadding capitalFirst minFont">
+<a href="<?= $backLink; ?>" class="centerHoriRowLayout minGap importantInput roundedMid tinHeight fitWidth regPadding capitalFirst minFont shadowed">
     <img src="../../Shared/Img/ArrowIcon.png" alt="Arrow" class="invertColors mirrorX">
     <h3><?= $lastPage ?> Page</h3>
 </a>
